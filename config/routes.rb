84761da84ac6a2aae921da7a974d827ae39ffd59
ToraJirou <31720141+Yuzpon@users.noreply.github.com>
2018-01-14 20:03:14 +0900

    Revert "Add aaaaaa"

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,8 @@ Rails.application.routes.draw do
     post   '/login',   to: 'sessions#create'
     delete '/logout',  to: 'sessions#destroy'
 
-
+    
     resources :users
-
-    # aaaa
 
 
 end
