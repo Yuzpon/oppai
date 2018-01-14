ef16211b5e8f979bfa46b83074938946d02bcadc
Yuzpon <wtr_0227@yahoo.co.jp>
2018-01-14 20:07:55 +0900

    aaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,10 @@ Rails.application.routes.draw do
     post   '/login',   to: 'sessions#create'
     delete '/logout',  to: 'sessions#destroy'
 
-    
+
     resources :users
+
+    # oppai
 
 
 end
